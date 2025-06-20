010758afc873b7c8a57e83a63e09057aa49156b9
LDanic <vadani1234@gmail.com>
2025-06-20 17:12:37 -0500

    modales añadir tarea y añadir issues

--- src/components/Issues.jsx
+++ src/components/Issues.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import addIcon from '../assets/addIcon.png';
 import avatarMale from '../assets/avatarMale.png';
 import avatarFemale from '../assets/avatarFemale.png';
 import Table from "./Table";
 import EstadoBadgeFactory from "../utils/EstadoBadgeFlyweight.jsx";
+import ModalAddTI from "./ModalAddTI";
 import "../styles/Issues.css";
 
 function Issues() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
     const columns = [
         { key: "nombre", title: "Nombre", width: "1fr" },
         {
@@ -45,6 +48,9 @@ function Issues() {
         },
     ];
 
+    const handleSave = (newIssue) => {
+        console.log("Tarea creada:", newIssue);
+    };
 
     return (
         <div className="issues-container">
@@ -52,11 +58,22 @@ function Issues() {
             <Table columns={columns} data={data} />;
 
             <div className="buttons-wrapper">
-                <button type="button" className="button">
+                <button type="button" className="button" onClick={() => setIsModalOpen(true)}>
                     <img src={addIcon} className="icon" alt="Añadir" />
                     <div className="text-button">Añadir Issue</div>
                 </button>
             </div>
+
+            {isModalOpen && (
+                <ModalAddTI
+                    tipo="Issue"
+                    onClose={() => setIsModalOpen(false)}
+                    onSave={(issue) => {
+                        handleSave(issue);
+                        setIsModalOpen(false);
+                    }}
+                />
+            )}
 
         </div>
     );
